5cf45c5328f32df38fb28a83adabd58328a14cdc
김지슬 <wltmfdl123@naver.com>
2021-10-06 02:11:19 +0900

    211006 || solved programmers

--- 프로그래머스/2018 KAKAO BLIND RECRUITMENT/1차_방금그곡.py
+++ 프로그래머스/2018 KAKAO BLIND RECRUITMENT/1차_방금그곡.py
@@ -1,37 +1,41 @@
+# '#' 처리 (소문자로 바꿔준다.)
+def change_sharp(melody):
+    melody = melody.replace('C#', 'c')
+    melody = melody.replace('D#', 'd')
+    melody = melody.replace('F#', 'f')
+    melody = melody.replace('G#', 'g')
+    melody = melody.replace('A#', 'a')
+    melody = melody.replace('B#', 'b')
+    return melody
+
+
+# 받아온 시간을 분 단위로 반환해주는 함수
 def play_time(start, end):
-    sh, sm = map(int, start.split(':'))
-    eh, em = map(int, end.split(':'))
-    if sm < em:
-        return (em - sm) + (eh - sh)
-    else:
-        return (sm - em) + (eh - sh - 1)
-
-
-def is_melody(m, melody, ml):
-    s = 0
-    for i in range(len(melody)):
-        if melody[i] == m[s]:
-            s += 1
-        else:
-            s = 0
-        if s == ml:
-            if melody[s] != '#':
-                return True
-    return False
+    sh, sm = map(int, start.split(':'))  # 시작 시, 분
+    eh, em = map(int, end.split(':'))  # 종료 시, 분
+    return (eh - sh) * 60 + (em - sm)
 
 
 def solution(m, musicinfos):
-    ml = len(m)
-    melody_can = []
+    m = change_sharp(m)  # 네오가 기억한 멜로디
+
+    melody_candi = []  # 방송된 곡들 중, 일치하는 곡들의 정보 담을 리스트
     for info in musicinfos:
         info = info.split(',')
-        pt = play_time(info[0], info[1])
+        pt = play_time(info[0], info[1])  # 재생시간 분 단위로 계산
 
-        if is_melody(m, info[3] * pt, ml-1):
-            melody_can.append((pt, info[2]))
+        melody = change_sharp(info[3])  # 악보 정보
+        q, r = divmod(pt, len(melody))
 
-    melody_can.sort(reverse=True)
-    return melody_can[0][1]
+        if m in melody * q + melody[:r]:  # 재생된 악보 중에 기억한 멜로디 있다면
+            melody_candi.append((pt, info[2]))  # 후보에 추가
+
+    if melody_candi:
+        melody_candi.sort(key=lambda x: x[0], reverse=True)     # 재생시간이 긴 순으로 정렬 (재생시간만 정렬)
+        return melody_candi[0][1]    # 맨 앞의 제목 return
+    return "(None)"   # 일치하는 음악 없을 경우
 
 
-print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))  # WORLD
+print(solution("CCB", ["03:00,03:10,FOO,CCB#CCB", "04:00,04:08,BAR,ABC"])) # FOO
+print(solution("ABC", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:14,WORLD,ABCDEF"]))   # HELLO
--- 프로그래머스/2018 KAKAO BLIND RECRUITMENT/1차_캐시.py
+++ 프로그래머스/2018 KAKAO BLIND RECRUITMENT/1차_캐시.py
@@ -1,22 +1,22 @@
 def solution(cacheSize, cities):
-    cities = list(map(lambda x: x.lower(), cities))
+    cities = list(map(lambda x: x.lower(), cities)) # 다 소문자 변환
     time = 0
     cache = []
     for city in cities:
-        if city in cache:
+        if city in cache:   # cache hit인 경우
             time += 1
-            cache.remove(city)
+            cache.remove(city)  # LRU 이므로 캐시 리스트 맨 마지막으로 갱신시킨다.
             cache.append(city)
-        else:
+        else:               # cache miss인 경우
             time += 5
             cache.append(city)
-            if cacheSize < len(cache):
+            if cacheSize < len(cache):  # cashe가 다 채워지지 않는 경우도 있으므로 조건문 달아줘야함
                 del cache[0]
 
     return time
 
 
-####################################################
+###################### deque 사용해서 풀어봄 근데 더 느렸음 ###############################
 import collections
 
 
