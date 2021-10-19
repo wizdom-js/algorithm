4ede36184cd8efb7a77476402d3d9e65d03b72e2
김지슬 <wltmfdl123@naver.com>
2021-10-19 23:50:54 +0900

    211019 || solved BOJ_BFS

--- 알고리즘 스터디/DFS&BFS/9205_맥주마시면서걸어가기.py
+++ 알고리즘 스터디/DFS&BFS/9205_맥주마시면서걸어가기.py
@@ -1,12 +1,48 @@
 import sys
 sys.stdin = open('input.txt')
 
+from collections import deque
+
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+def bfs(sy, sx):
+    queue = deque([sy, sx, 19])
+    while queue:
+        y, x, beer = deque.popleft()
+        if beer == 0:
+            continue
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < 32767 and 0 <= nx < 32767 and not visited[ny][nx]:
+                if point[ny][nx] == 'p':
+                    visited[ny][nx] = 1
+                    queue.append([ny, nx, 19])
+                elif point[ny][nx] == 'e':
+                    return 'happy'
+                else:
+                    visited[ny][nx] = 1
+                    queue.append([ny, nx, beer-1])
+    return 'sad'
+
+
 
 tc = int(input())
 for tc_n in range(1, tc+1):
     n = int(input())
-    coordinate = [[0, 0] for _ in range(n+2)]
+    point = [[0 for _ in range(32767)] for _ in range(32767)]
     for i in range(n+2):
-        coordinate[i] = list(map(int, input().split()))
+        if i == 0:
+            sx, sy = map(int, input().split())
+            point[sy//50][sx//50] = 's'
+        elif i == n+1:
+            x, y = map(int, input().split())
+            point[y//50][x//50] = 'e'
+        else:
+            x, y = map(int, input().split())
+            point[y//50][x//50] = 'p'
 
-        
+    visited = [[0 for _ in range(32767)] for _ in range(32767)]
+
+    print(bfs(sy, sx))
