bacbbc9c4918fa75f4e4ca3e2d829558d341162e
김지슬 <wltmfdl123@naver.com>
2021-10-21 22:58:09 +0900

    211021 || added explanation

--- 알고리즘 스터디/DFS&BFS/2589_보물섬.py
+++ 알고리즘 스터디/DFS&BFS/2589_보물섬.py
@@ -3,33 +3,41 @@ sys.stdin = open('input.txt')
 
 from collections import deque
 
+# 상 하 좌 우
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 
 def bfs(sy, sx, hour):
-    queue = deque([(sy, sx, hour)])
-    visited = [[0 for _ in range(garo)] for _ in range(sero)]
-    visited[sy][sx] = 1
+    queue = deque([(sy, sx, hour)]) # 시작지점 추가하고 시작
+    visited = [[0 for _ in range(garo)] for _ in range(sero)]   # 방문한 곳 다시 방문하지 못하도록 만든 리스트
+    visited[sy][sx] = 1  # 시작지점 방문처리
     while queue:
-        y, x, hour = queue.popleft()
+        y, x, hour = queue.popleft()    # 좌표 하나 가져오기
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if 0 <= ny < sero and 0 <= nx < garo:
-                if treasure_map[ny][nx] == 'W' or visited[ny][nx]:
+            if 0 <= ny < sero and 0 <= nx < garo:   # 다음 갈 곳이 지도 범위를 벗어나지 않는다면
+                if treasure_map[ny][nx] == 'W' or visited[ny][nx]:  # 다음 갈 곳이 바다이거나 방문했던 곳이면 가지않도록
                     continue
-                visited[ny][nx] = 1
-                queue.append((ny, nx, hour+1))
-    return hour
+                visited[ny][nx] = 1 # 방문처리
+                queue.append((ny, nx, hour+1))  # queue에 가볼 곳으로 추가하기
+    return hour # 시작지점에서 막다른 곳까지 걸리는 시간 return
 
 
-sero, garo = map(int, input().split())
-treasure_map = [input() for _ in range(sero)]
+sero, garo = map(int, input().split())          # 지도의 세로 길이, 가로 길이
+treasure_map = [input() for _ in range(sero)]   # 보물 지도
 
 answer = 0
 for h in range(sero):
+    gap = 0
     for w in range(garo):
-        if treasure_map[h][w] == 'L':
-            answer = max(answer, bfs(h, w, 0))
-
+        if gap:
+            gap -= 1
+            continue
+        if treasure_map[h][w] == 'L':   # 해당 좌표 육지이면
+            tmp = bfs(h, w, 0)
+            if answer < tmp:  # 탐색해보는데, 더 긴 시간으로 갱신
+                answer = tmp
+            else:
+                gap = answer - tmp
 print(answer)
--- 알고리즘 스터디/DFS&BFS/9205_맥주마시면서걸어가기.py
+++ 알고리즘 스터디/DFS&BFS/9205_맥주마시면서걸어가기.py
@@ -3,6 +3,7 @@ sys.stdin = open('input.txt')
 
 from collections import deque
 
+'''
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 
@@ -46,6 +47,50 @@ for tc_n in range(1, tc+1):
     visited = [[0 for _ in range(400)] for _ in range(400)]
 
     print(bfs(sy, sx))
+
+'''
+####################################################
+
+
+def bfs(sx, sy):
+    queue = deque([(sx, sy)])   # 출발지점 넣기
+    visited[(sx, sy)] = False    # 출발지점 방문 처리
+    while queue:
+        x, y = queue.popleft()  # 좌표 하나 빼
+        if abs(ex-x) + abs(ey-y) <= 1000:   # 현재 지점에서 도착지점까지 맥주 20개로 갈 수 있다면
+            return 'happy'  # 'happy' 출력
+
+        for i in range(len(cu)):
+            cu_x = cu[i][0] # 편의점 가보기
+            cu_y = cu[i][1]
+            # 현재 편의점에서 목적지까지 맥주 20개로 갈 수 있고 방문해본 편의점이 아니라면
+            if abs(cu_x-x) + abs(cu_y-y) <= 1000 and visited.get((cu_x, cu_y), True):
+                visited[(cu_x, cu_y)] = False   # 방문처리
+                queue.append((cu_x, cu_y))      # 방문해보자 queue에 넣어
+    return 'sad'    # 출발 그리고 편의점에서도 맥주 20개로 목적지까지 못가면 'sad'
+
+
+tc = int(input())
+for tc_n in range(1, tc+1):
+    n = int(input())    # 편의점 개수
+
+    sx, sy = map(int, input().split())  # 출발 좌표
+    cu = [] # 편의점 좌표 저장
+    for i in range(n):
+        cu.append(list(map(int, input().split())))
+    ex, ey = map(int, input().split())  # 페스티벌 좌표
+
+    visited = {}
+
+    print(bfs(sx, sy))
+
+
+
+
+
+
+
+
 
 
 '''
