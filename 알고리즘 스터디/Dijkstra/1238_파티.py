a019036b486f85294e3387564b87363c329799d2
김지슬 <wltmfdl123@naver.com>
2021-11-10 23:39:38 +0900

    211110 || solved BOJ_Dijkstra

--- 알고리즘 스터디/Dijkstra/1238_파티.py
+++ 알고리즘 스터디/Dijkstra/1238_파티.py
@@ -14,30 +14,32 @@ def dijkstra(s, e):
         if now == e:
             return d
 
-        if d > dist[now]:
+        if dist[now] < d:
             continue
 
-        for i in visited[now]:
-            nd = dist[now] + i[1]
+        for v in visited[now]:
+            nd = v[1] + d
 
-            if dist[i[0]] > nd:
-                dist[i[0]] = nd
-                heapq.heappush(que, [nd, i[0]])
+            if nd < dist[v[0]]:
+                dist[v[0]] = nd
+                heapq.heappush(que, [nd, v[0]])
+
 
 N, M, X = map(int, input().split()) # 학생 N명, 단방향 도로 개수 M, 파티 벌이는 마을 X
 
-
-dist = [float('inf') for i in range(N + 1)]
-visited = [[] for i in range(N+1)]
-for i in range(N):
+dist = [float('inf') for _ in range(N + 1)]
+visited = [[] for _ in range(N+1)]
+for i in range(M):
     s, e, t = map(int, input().split()) # 도로의 시작점 s, 끝점 e, 도로를 지나는 소요시간 t
     visited[s].append([e, t])
 
-print(visited)
-
+time = [0 for _ in range(N+1)]
 for i in range(1, N+1):
-    answer = dijkstra(i, e) + dijkstra(e, i)
-    print(answer)
+    dist = [float('inf') for _ in range(N + 1)]
+    time[i] += dijkstra(i, X)
 
+    dist = [float('inf') for _ in range(N + 1)]
+    time[i] += dijkstra(X, i)
 
-print(dist)
+print(max(time))
+
