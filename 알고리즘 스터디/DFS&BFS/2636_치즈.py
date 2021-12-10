ec579907c5fbb8ebe7277f549a40a922de146bfa
김지슬 <wltmfdl123@naver.com>
2021-12-10 23:57:22 +0900

    211210 || BOJ 문제 주석 달기

--- 알고리즘 스터디/DFS&BFS/2636_치즈.py
+++ 알고리즘 스터디/DFS&BFS/2636_치즈.py
@@ -7,36 +7,36 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def bfs():
-    visited = [[False for _ in range(w)] for _ in range(h)]
-    queue = deque([(0, 0)])
-    melted = 0
+    visited = [[False for _ in range(w)] for _ in range(h)] # 방문처리 배열
+    queue = deque([(0, 0)]) # 처음 위치 0,0
+    melted = 0              # 녹인 치즈 개수
     while queue:
-        y, x = queue.popleft()
+        y, x = queue.popleft()  # 이동할 곳 가져오기
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if 0 <= ny < h and 0 <= nx < w:
-                if cheese[ny][nx] == 0 and not visited[ny][nx]:
-                    visited[ny][nx] = True
-                    queue.append((ny, nx))
-                elif cheese[ny][nx] == 1:
-                    cheese[ny][nx] = 0
+            if 0 <= ny < h and 0 <= nx < w: # 움직일 곳이 범위 내라면
+                if cheese[ny][nx] == 0 and not visited[ny][nx]: # 판(0)이고, 방문하지 않은 곳이라면
+                    visited[ny][nx] = True  # 방문처리
+                    queue.append((ny, nx))  # 해당 위치에서 검토해보기 위해 queue에 넣어줌 (판에서 만난 치즈는 다 가장자리 치즈이므로)
+                elif cheese[ny][nx] == 1:   # 가장자리 치즈라면
+                    cheese[ny][nx] = 0      # 녹이기
                     melted += 1
                     visited[ny][nx] = True
-    return melted
+    return melted   # 녹인 치즈 개수 반환
 
 
-h, w = map(int, input().split())
-cheese = [list(map(int, input().split())) for _ in range(h)]
+h, w = map(int, input().split())    # 치즈판의 높이와 너비
+cheese = [list(map(int, input().split())) for _ in range(h)]    # 받아온 치즈 판
 
-time = 0
-mc = 0
+time = 0    # 모든 치즈를 녹이는데 걸린 시간
+mc = 0      # 1시간마다 녹은 치즈 개수를 저장할 변수 (1시간 전 치즈 개수 반환해야하므로)
 while True:
-    tmp = bfs()
-    if tmp == 0:
+    tmp = bfs()     # 치즈 녹이기
+    if tmp == 0:    # 만약 녹일 치즈가 없다면 끝내기
         break
-    mc = tmp
-    time += 1
+    mc = tmp        # 녹은 치즈 있었다면 개수 저장해놓기
+    time += 1       # 시간 + 1
 
 print(time)
 print(mc)
--- 알고리즘 스터디/구현/1063_킹.py
+++ 알고리즘 스터디/구현/1063_킹.py
@@ -1,7 +1,7 @@
 import sys
 sys.stdin = open('input.txt')
 
-
+# 명령에 따른 움직임
 def move(command):
     if command == 'R':
         return (1, 0)
@@ -21,23 +21,23 @@ def move(command):
         return (-1, -1)
 
 
-king_point, rock_point, n = input().split()
-kp_x, kp_y = ord(king_point[0]), int(king_point[1])
-rp_x, rp_y = ord(rock_point[0]), int(rock_point[1])
-king_move = [input() for _ in range(int(n))]
+king_point, rock_point, n = input().split() # king 위치, 돌 위치, 킹 움직인 횟수 n
+kp_x, kp_y = ord(king_point[0]), int(king_point[1]) # king의 위치 x, y 분리
+rp_x, rp_y = ord(rock_point[0]), int(rock_point[1]) # 돌의 위치 x, y 분리
+king_move = [input() for _ in range(int(n))]    # king 움직임 리스트로 받기
 
 for command in king_move:
-    x, y = move(command)
-    nx = kp_x + x
+    x, y = move(command)    # 움직인 명령 받기
+    nx = kp_x + x   # 명령 받은대로 위치 이동
     ny = kp_y + y
-    if 65 <= nx <= 72 and 1 <= ny <= 8:
-        if rp_x == nx and rp_y == ny:
-            if 65 <= rp_x + x <= 72 and 1 <= rp_y + y <= 8:
-                kp_x, kp_y = nx, ny
-                rp_x, rp_y = rp_x + x, rp_y + y
+    if 65 <= nx <= 72 and 1 <= ny <= 8: # 움직일 위치가 판 범위 내에 있다면
+        if rp_x == nx and rp_y == ny:   # 근데 돌의 위치랑 같아 졌다면
+            if 65 <= rp_x + x <= 72 and 1 <= rp_y + y <= 8: # 돌을 왕이 움직인 대로 움직이고, 그 위치가 판 내라면
+                kp_x, kp_y = nx, ny             # 왕 이동
+                rp_x, rp_y = rp_x + x, rp_y + y # 돌 이동
         else:
-            kp_x, kp_y = nx, ny
+            kp_x, kp_y = nx, ny # 돌 위치랑 안같다면 왕만 이동
 
 
-print(chr(kp_x) + str(kp_y))
-print(chr(rp_x) + str(rp_y))
+print(chr(kp_x) + str(kp_y))    # king 마지막 위치
+print(chr(rp_x) + str(rp_y))    # 돌 마지막 위치
