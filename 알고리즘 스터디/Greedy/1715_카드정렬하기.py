ff525ffedab7bc306ea830be522f220feace6ccd
김지슬 <wltmfdl123@naver.com>
2021-12-11 23:53:45 +0900

    211211 || BOJ 주석 달기

--- 알고리즘 스터디/Greedy/1715_카드정렬하기.py
+++ 알고리즘 스터디/Greedy/1715_카드정렬하기.py
@@ -3,17 +3,20 @@ sys.stdin = open('input.txt')
 
 import heapq
 
-n = int(input())
+n = int(input())    # 카드 묶음 수
+
+# 입력받은 카드 묶음 정렬 - 1. 힙으로 정렬
 cards = [int(input()) for _ in range(n)]
 heapq.heapify(cards)
 
+# 입력받은 카드 묶음 정렬 - 2. sorted
 # cards = sorted([int(input()) for _ in range(n)])
 
 answer = 0
-while len(cards) > 1:
-    n1 = heapq.heappop(cards)
-    n2 = heapq.heappop(cards)
-    heapq.heappush(cards, n1 + n2)
-    answer += n1 + n2
+while len(cards) > 1:   # 카드 비교 할 수 없을 때까지 반복
+    n1 = heapq.heappop(cards)   # 카드리스트에서 가장 작은 카드 묶음 꺼내기
+    n2 = heapq.heappop(cards)   # 그다음으로 작은 카드 묶음 꺼내기
+    heapq.heappush(cards, n1 + n2)  # 두 카드 묶음 합을 카드리스트 크기에 맞게 넣기
+    answer += n1 + n2   # 카드 비교 횟수 더해주기
 
 print(answer)
--- 알고리즘 스터디/UF/15809_전국시대.py
+++ 알고리즘 스터디/UF/15809_전국시대.py
@@ -8,51 +8,56 @@ sys.stdin = open('input.txt')
 #     parent[x] = find(parent[x])
 #     return parent[x]
 
+# 부모 찾기
 def find(x):
     if parent[x] != x:
         parent[x] = find(parent[x])
     return parent[x]
 
-
+# 합치기
 def union(o, x, y):
     x = find(x)
     y = find(y)
 
-    if o == 1:
-        parent[x] = y
-        mp[y] += mp[x]
+    if o == 1:  # 동맹일때
+        parent[x] = y   # 아무곳에나 합치고
+        mp[y] += mp[x]  # 병력도 합치기
         return
 
+    # 전쟁일 때
+    # 병력이 더 많은 나라에 붙이고, 병력 수치 빼주기
     if mp[x] < mp[y]:
         parent[x] = y
         mp[y] -= mp[x]
     elif mp[y] < mp[x]:
         parent[y] = x
         mp[x] -= mp[y]
-    else:
+    else:   # 두 나라의 병력이 같을 경우 멸망
         parent[x] = 0
         parent[y] = 0
 
 
-n, m = map(int, input().split())
-mp = [0] + [int(input()) for _ in range(n)]
-parent = list(range(n+1))
+n, m = map(int, input().split())    # n개의 국가, m개의 기록
+mp = [0] + [int(input()) for _ in range(n)]     # 각 국가의 병력의 수
+parent = list(range(n+1))   # 부모 찾기 리스트
 
 for _ in range(m):
-    o, p, q = map(int, input().split())
-    union(o, p, q)
+    o, p, q = map(int, input().split()) # 기록 받아오기
+    union(o, p, q)  # 합쳐주기
 
+# 모든 국가의 각 부모 찾아주기
+# (정리해주는 느낌 (반영 안된 나라도 있으므로))
 for i in range(1, n+1):
     parent[i] = find(parent[i])
 
 answer = []
-for i in set(parent):
-    if i == 0:
+for i in set(parent):   # 중복 거르기
+    if i == 0:  # 멸망한 나라 제외
         continue
-    answer.append(mp[i])
+    answer.append(mp[i])    # 남아있는 국가의 병력의 수 리스트에 넣어주기
 
-answer.sort()
+answer.sort()   # 오름차순으로 정렬
 
-print(len(answer))
+print(len(answer)) 
 print(*answer)
 
