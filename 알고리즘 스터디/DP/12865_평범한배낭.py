2e8fc093f895e221ec1ce0a7904f46beca0fd829
김지슬 <wltmfdl123@naver.com>
2021-12-13 23:29:12 +0900

    211213 || solved BOJ_DP

--- 알고리즘 스터디/DP/12865_평범한배낭.py
+++ 알고리즘 스터디/DP/12865_평범한배낭.py
@@ -2,19 +2,18 @@ import sys
 sys.stdin = open('input.txt')
 
 n, k = map(int, input().split())    # 물품의 수 n, 버틸 수 있는 무게 k
-arr = [list(map(int, input().split())) for _ in range(n)]    # [물건의 무게, 물건의 가치]
-arr.sort()
+arr = [[0, 0]] + [list(map(int, input().split())) for _ in range(n)]    # [물건의 무게, 물건의 가치]
 
-dp = [0 for _ in range(100)]
-dp_v = [0 for _ in range(100)]
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
 
-for thing in arr:
-    w = thing[0]    # 물건의 무게 w
-    v = thing[1]    # 물건의 가치 v
-    # dp[w] = max(dp[w], v)
-    for i in range(w, k-w+1):
-        dp[i] = max(v, dp[i-1])
+for i in range(1, n+1):
+    w = arr[i][0]
+    v = arr[i][1]
+    for j in range(1, k+1):
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
 
-print(dp)
-print(dp[k])
+print(dp[n][k])
 
