845f7ee5f9593744332b0b0d1eb8d7e503c4835b
김지슬 <wltmfdl123@naver.com>
2021-12-22 23:50:47 +0900

    211222 || solved BOJ_자료구조

---
+++ 알고리즘 스터디/자료구조/2346_풍선터뜨리기.py
@@ -0,0 +1,86 @@
+import sys
+sys.stdin = open('input.txt')
+#
+# n = int(input())    # 풍선의 개수
+# papers = list(map(int, input().split()))    # 풍선 안의 종이에 적혀 있는 수
+#
+# p = papers[0]
+# cnt = 0
+# move = 0
+# direction = True
+# print(1, end=' ')
+# papers[0] = 0
+# while cnt < n:
+#     if papers[p] == 0 and cnt > 0:
+#         if direction:
+#             p += 1
+#         else:
+#             p -= 1
+#     else:
+#
+#         print(p + 1, end=' ')
+#
+#         cnt += 1
+#         move = papers[p]
+#         direction = True if move > 0 else False
+#         papers[p] = 0
+#         p += move
+#         if p < 0:
+#             p += n
+#         else:
+#             p %= n
+
+def num(p):
+    if p < 0:
+        p += n
+    else:
+        p %= n
+    return p
+
+
+
+
+
+# n = int(input())    # 풍선의 개수
+# papers = list(map(int, input().split()))    # 풍선 안의 종이에 적혀 있는 수
+#
+# p = cnt = move = 0
+# direction = True
+# while cnt < n:
+#     if papers[p] == 0:
+#         if direction:
+#             p += 1
+#         else:
+#             p -= 1
+#     else:
+#         print(p + 1, end=' ')
+#
+#         cnt += 1
+#         move = papers[p]
+#         direction = True if move > 0 else False
+#         papers[p] = 0
+#         p += move
+#     p = num(p)
+
+
+
+n = int(input())     # 풍선의 개수
+papers = list(enumerate(map(int, input().split()))) # 풍선 안의 종이에 적혀 있는 수
+
+p = 1
+move = 0
+while n:
+    p += move
+    if move < 0:
+        p %= n
+    else:
+        p = (p - 1) % n
+    idx, move = papers.pop(p)
+    n -= 1
+    print(idx + 1, end=' ')
+
+
+
+
+
+
