3477e42c7e80bd71813c575e95b63b3a0c004123
김지슬 <wltmfdl123@naver.com>
2022-01-01 23:37:53 +0900

    220101 || 알고리즘 복습 & 주석

--- 알고리즘 스터디/Dijkstra/14496_그대,그머가되어.py
+++ 알고리즘 스터디/Dijkstra/14496_그대,그머가되어.py
@@ -4,36 +4,36 @@ sys.stdin = open('input.txt')
 import heapq
 
 def dijkstra():
-    dist = [float('inf') for _ in range(n+1)]
-    queue = []
-    heapq.heappush(queue, [0, a])
-    dist[a] = 0
+    dist = [float('inf') for _ in range(n+1)]   # 가중치 초기화
+    queue = [[0, a]]    # 횟수, 시작 문자 넣기
+    dist[a] = 0     # 시작지점 가중치 0
     while queue:
-        d, now = heapq.heappop(queue)
+        d, now = heapq.heappop(queue)   # 현재까지 치환 횟수, 현재 문자 뽑기
 
-        if now == b:
-            print(d)
+        if now == b:    # 바꾸려고 하는 문자 b라면
+            print(d)    # 현재까지 치환 횟수 출력
             return
 
-        if d < dist[now]:
+        if dist[now] < d:   # 저장한 치환횟수보다 꺼낸게 더 크다면 패스
             continue
 
-        for next in visited[now]:
-            nd = d + 1
-            if nd < dist[next]:
+        for next in visited[now]:   # 현재 문자에서 바꿀 수 있는 문자로 가보기
+            nd = d + 1  # 치환 횟수 + 1
+            if nd < dist[next]: # 저장한 치환 횟수보다 적다면 갱신
                 dist[next] = nd
-                heapq.heappush(queue, [nd, next])
-    print(-1)
+                heapq.heappush(queue, [nd, next])   # 해당 문자로 가보기
+
+    print(-1)   # 문자 b로 바꿀 수 없다면 -1 출력
     return
 
 
-a, b = map(int, input().split())
-n, m = map(int, input().split())
+a, b = map(int, input().split())    # 바꾸려고 하는 문자 a, b
+n, m = map(int, input().split())    # 전체 문자의 수 n, 치환 가능한 문자쌍의 수 m
 
-visited = [[] for _ in range(n+1)]
+visited = [[] for _ in range(n+1)]  # 인접 리스트 만들기
 for _ in range(m):
-    t1, t2 = map(int, input().split())
-    visited[t1].append(t2)
+    t1, t2 = map(int, input().split())  # 바꿀 수 있는 두 문자
+    visited[t1].append(t2)  # 양방향 
     visited[t2].append(t1)
 
 dijkstra()
--- 알고리즘 스터디/정수론/20309_트리플소트.py
+++ 알고리즘 스터디/정수론/20309_트리플소트.py
@@ -32,15 +32,15 @@ sys.stdin = open('input.txt')
 
 
 n = int(input())
-numbers = list(enumerate(map(int, input().split())))
+numbers = list(enumerate(map(int, input().split())))   # (인덱스, 숫자) 같이 저장
 print(numbers)
-numbers.sort(key=lambda x: x[1])
+numbers.sort(key=lambda x: x[1])    # 숫자 기준으로 정렬
 print(numbers)
 
-is_even = True
+is_even = True  # 홀수 순서, 짝수 순서 판별
 for idx, num in numbers:
-    if (idx % 2 and is_even) or (not idx % 2 and not is_even): # 홀수, 짝수
-        print('NO')
+    if (idx % 2 and is_even) or (not idx % 2 and not is_even): # 인덱스가 홀수인데 짝수 순서이거나, 인덱스가 짝수인데 홀수 순서라면
+        print('NO') # 정렬 불가능
         break
 
     is_even = not is_even
@@ -49,12 +49,13 @@ else:
     print('YES')
 
 
-n = int(input())
-numbers = list(map(int, input().split()))
+n = int(input())    # 배열의 크기
+numbers = list(map(int, input().split()))   # 배열의 원소
 for i in range(n):
-    if i % 2:
-        if numbers[i] % 2:
-            print('NO')
+    # 짝수번째 값이 모두 짝수여야 정렬 가능
+    if i % 2:   # 짝수 자리라면 (0부터 시작하므로 사실상 짝수 자리)
+        if numbers[i] % 2:  # 근데 원소가 홀수라면
+            print('NO') # 정렬 불가능
             exit()
 else:
-    print('YES')
+    print('YES')    # 그 외는 정렬 가능
