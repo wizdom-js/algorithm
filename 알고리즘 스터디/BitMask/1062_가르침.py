55e7970a2f4522a5b3fcfd599dc1dbeded3744fb
김지슬 <wltmfdl123@naver.com>
2022-01-13 23:35:52 +0900

    220113 || solved BOJ_Bit Mask

--- 알고리즘 스터디/BitMask/1062_가르침.py
+++ 알고리즘 스터디/BitMask/1062_가르침.py
@@ -1,6 +1,61 @@
 import sys
 sys.stdin = open('input.txt')
 
-n, k = map(int, input().split())
-words = list(input().split())
+# dy = [1, -1, 0, 0]
+# dx = [0, 0, 1, -1]
 
+n, k = map(int, input().split())
+words = [list(set(input())) for _ in range(n)]
+# word_mx_idx = [0 for _ in range(n)]
+# for i in range(n):
+#     word_mx_idx[i] = len(words[i])-1
+
+visit = 0
+for word in ['a', 'c', 'i', 'n', 't']:
+    visit |= (1 << (ord(word) - 97))
+
+answer = 0
+def read_cnt(visit):
+    cnt = 0
+    for i in range(n):
+        for w in words[i]:
+            if not visit & (1 << (ord(w) - 97)):
+                break
+        else:
+            cnt += 1
+
+    return cnt
+
+def dfs(idx, cnt, visit):
+    global answer
+
+    if cnt == k-5:
+        tmp = read_cnt(visit)
+        if answer < tmp:
+            answer = tmp
+        return
+
+    for i in range(idx, 26):
+        bit = 1 << i
+        if visit & bit:
+            continue
+        dfs(i, cnt + 1, visit | bit)
+        # visit &= ~bit
+
+dfs(0, 0, visit)
+print(answer)
+
+
+# for i in range(4):
+#     ny = y + dy[i]
+#     nx = x + dx[i]
+#     if 0 <= ny < n and 0 <= nx < word_mx_idx[ny]:
+#         bit = (1 << (ord(words[ny][nx]) - 97))
+#         words[ny][nx] = 0
+#         if visit & bit:
+#             # if words[ny][nx]:
+#             #     words[ny][nx] = 0
+#             continue
+#         dfs(ny, nx, cnt + 1, visit | bit)
+#         visit &= ~bit
+#         words[ny][nx] = chr(bit + 96)
