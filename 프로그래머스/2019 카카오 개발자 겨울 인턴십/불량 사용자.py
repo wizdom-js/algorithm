ee9336a6add3dce57628dd5683aa04e00c7f8d2e
wizdom-js <wltmfdl123@naver.com>
2022-07-06 23:59:18 +0900

    [Algorithm] 추가 풀이

--- 프로그래머스/2019 카카오 개발자 겨울 인턴십/불량 사용자.py
+++ 프로그래머스/2019 카카오 개발자 겨울 인턴십/불량 사용자.py
@@ -26,3 +26,47 @@ def solution(user_id, banned_id):
  
  
     return len(answer)
+
+def solution(user_id, banned_id):
+    ban_candidate = [[] for _ in range(len(banned_id))]
+    
+    
+    # 밴 아이디 적용되는거 등록
+    for u in range(len(user_id)):
+        u_id = user_id[u]
+        for b in range(len(banned_id)):
+            b_id = banned_id[b]
+            len_b_id = len(b_id)
+            
+            # 밴 아이디와 유저 아이디 길이 다르면 패스 
+            if len_b_id != len(u_id):
+                continue
+                
+            for i in range(len_b_id):
+                if b_id[i] != u_id[i] and b_id[i] != '*':
+                    break
+            else:
+                ban_candidate[b].append(u)
+    
+    print(ban_candidate)
+    
+    answer = 0
+    def dfs(idx):
+        nonlocal check, answer
+        if idx == len(user_id):
+            print(check)
+            if sum(check) == 0:
+                answer += 1
+            return
+        
+        for i in ban_candidate[idx]:
+            if not check[i]:
+                continue
+            check[i] = False
+            dfs(idx+1)
+            check[i] = True
+    
+    check = [1 for _ in range(len(user_id))]
+    dfs(0)
+            
+    return answer
