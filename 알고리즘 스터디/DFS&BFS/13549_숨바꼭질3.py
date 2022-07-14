aef72204615961a1df4cf8db3f1f0f9769f77d67
wizdom-js <wltmfdl123@naver.com>
2022-07-14 23:51:23 +0900

    [Annotation] 코드 주석 달기

--- 알고리즘 스터디/DFS&BFS/13549_숨바꼭질3.py
+++ 알고리즘 스터디/DFS&BFS/13549_숨바꼭질3.py
@@ -3,30 +3,35 @@ sys.stdin = open('input.txt')
 
 from collections import deque
 
-subin, sister = map(int, input().split())
-visited = [0 for _ in range(100001)]
+# 목적지까지 최단거리(시간)을 구하는 것이기 때문에 bfs => 도착하자마자 탐색 종료하면 더 깊은 depth까지 가지 않아도 되므로
 
-queue = deque([[subin, 0]])
+subin, sister = map(int, input().split())   # 수빈이와 동생의 위치
+visited = [0 for _ in range(100001)]    # 방문처리 할 리스트
+
+queue = deque([[subin, 0]]) # 수빈이가 이동 할 곳
 while queue:
     now, time = queue.popleft()
-    visited[now] = True
+    visited[now] = True # 수빈 지금 위치 방문 처리
 
-    if now == sister:
+    if now == sister:   # 동생을 만난 경우
         print(time)
         break
 
-    if now < 50001 and not visited[now*2]:
+    # 위치 조건 0 <= x <= 100,000
+    # 방문하지 않은 곳 가기
+    if now < 50001 and not visited[now*2]:  # 순간이동
         queue.append([now*2, time])
-    if now > 0 and not visited[now-1]:
+    if now > 0 and not visited[now-1]:  # -1로 이동
         queue.append([now-1, time+1])
-    if now < 100000 and not visited[now+1]:
+    if now < 100000 and not visited[now+1]: # +1로 이동
         queue.append([now+1, time+1])
+
 
 
 def another_way():
     subin, sister = map(int, input().split())
-    visited = [-1 for _ in range(100001)]
-    visited[subin] = 0
+    visited = [-1 for _ in range(100001)]   # 방문 리스트를 -1로 처리
+    visited[subin] = 0  # 처음 수빈이의 위치 0으로 처리하고 계속해서 더해나가는 방식
 
     queue = deque([subin])
     while queue:
--- 알고리즘 스터디/DFS&BFS/6603_로또.py
+++ 알고리즘 스터디/DFS&BFS/6603_로또.py
@@ -1,25 +1,28 @@
 import sys
 sys.stdin = open('input.txt')
 
+# 모든 경우를 구할 때 dfs
+
+# 조합 구현
 def dfs(s, depth):
-    if depth == 6:
+    if depth == 6:  # 로또 6개 골랐다면 출력
         print(*combination)
         return
 
-    for i in range(s, k):
-        combination[depth] = numbers[i]
-        dfs(i+1, depth+1)
+    for i in range(s, k):   # 중복 방지위해 s부터 시작 k개 까지
+        combination[depth] = numbers[i] # 해당 깊이 자리에 k개의 숫자 중, 고른 번호 넣기
+        dfs(i+1, depth+1)   # 다음 번호 고르러 가기
 
 
-combination = [0 for _ in range(6)]
+combination = [0 for _ in range(6)] # 고른 번호가 들어갈 리스트
 
 while True:
     test_case = list(map(int, input().split()))
-    if test_case[0] == 0:
+    if test_case[0] == 0:   # 입력의 마지막 줄이라면
         break
 
-    k = test_case[0]
-    numbers = test_case[1:]
+    k = test_case[0]    # 고른 수의 개수
+    numbers = test_case[1:] # 숫자들
 
     dfs(0, 0)
     print()
