e34dae646c5b8da3215283fd7d3cc44f803fd747
wizdom-js <wltmfdl123@naver.com>
2022-07-21 21:41:47 +0900

    Refactor : 코드 정리 (조건문 뺴기)

--- 프로그래머스/멀리 뛰기.py
+++ 프로그래머스/멀리 뛰기.py
@@ -1,13 +1,12 @@
 def solution(n):
-
-    if n<3:
+    if n < 3:
         return n
 
-    d=[0]*(n+1)
-    d[1]=1
-    d[2]=2
+    dp = [0 for _ in range(n + 1)]
+    dp[1] = 1
+    dp[2] = 2
 
-    for i in range(3,n+1):
-        d[i]=d[i-1]+d[i-2]
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
 
-    return d[n]%1234567
+    return dp[n]
