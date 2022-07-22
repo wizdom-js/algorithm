81fb6dbc25d183a69b1bc5be98e55a19256d7e0a
wizdom-js <wltmfdl123@naver.com>
2022-07-22 22:04:54 +0900

    Comment : DP 주석 달기

--- 프로그래머스/멀리 뛰기.py
+++ 프로그래머스/멀리 뛰기.py
@@ -1,11 +1,11 @@
 def solution(n):
-    if n < 3:
-        return n
-
-    dp = [0 for _ in range(n + 1)]
+    # n + 2로 한 이유 => n이 1인 경우 4번째 줄에서 에러 나기 떄문
+    # if n < 3 : return n 해도 되지만 코드 간결화를 위함
+    dp = [0 for _ in range(n + 2)]
     dp[1] = 1
     dp[2] = 2
 
+    # 점화식 => 현재 x칸을 뛰기 위한 방법의 수 = 이전 경우(x-1)의 수 + 전전 경우(x-2)의 수
     for i in range(3, n + 1):
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
 
