bd45bddb4a7cb8870b535acf9ff8a9302f0f406c
wizdom-js <wltmfdl123@naver.com>
2022-08-02 15:13:54 +0900

    Refactor : 코드 수정

--- JAVA/Programmers/모의고사.java
+++ JAVA/Programmers/모의고사.java
@@ -1,34 +1,35 @@
 class Solution {
-  public static int[] solution(int[] answers) {
-      int[] answer;
-      
-      int[] a = new int[]{1,2,3,4,5};
-      int[] b = new int[]{2,1,2,3,2,4,2,5};
-      int[] c = new int[]{3,3,1,1,2,2,4,4,5,5};
-      
-      int[] score = new int[3];
-
-      for(int i=0; i<answers.length; i++){
-          if(answers[i] == a[i%5])  score[0]++; 
-          if(answers[i] == b[i%8])  score[1]++;
-          if(answers[i] == c[i%10]) score[2]++;
-      }
-    
-      int max = 0;
-      for(int i=0; i<3; i++){
-          if(score[i] > max){ max = score[i]; }
-      }
-      
-      int maxCount = 0;
-      for(int i=0; i<3; i++){
-          if(score[i] == max){ maxCount++; } 
-      }
-
-      answer = new int[maxCount];
-      int idx = 0;
-      for(int i=0; i<3; i++){
-          if(score[i] == max) answer[idx++] = i+1;
-      }
-      return answer;
-  }
+    public int[] solution(int[] answers) {
+        int[] a = new int[]{1, 2, 3, 4, 5};
+        int[] b = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        int[] score = new int[3];
+        for(int i=0; i<answers.length; i++){
+            if (answers[i] == a[i%5]) score[0]++;
+            if (answers[i] == b[i%8]) score[1]++;
+            if (answers[i] == c[i%10]) score[2]++;
+        }
+        
+        int maxCorrect = 0;
+        for(int i : score){
+            if (maxCorrect < i) maxCorrect = i;
+        }
+        
+        int maxCount = 0;
+        for(int i : score){
+            if (i == maxCorrect) maxCount++;
+        }
+        
+        int answer[] = new int[maxCount];
+        int idx = 0;
+        for(int i=0; i<3; i++){
+            if (score[i] == maxCorrect) {
+                answer[idx] = i+1;
+                idx++;
+            }
+        }
+        
+        return answer;
+    }
 }
