75d9686b4393f66c68f63cd6bf36aa3a8a782747
wizdom-js <wltmfdl123@naver.com>
2022-08-09 23:47:35 +0900

    Java : Hash Set_Programmers

---
+++ JAVA/Programmers/폰켓몬.java
@@ -0,0 +1,15 @@
+public int solution(int[] nums) {
+  int max = nums.length / 2;
+
+  HashSet<Integer> numsSet = new HashSet<>();
+
+  for (int num : nums) {
+    numsSet.add(num);
+  }
+
+  if (numsSet.size() > max) {
+    return max;
+  } else {
+    return numsSet.size();
+  }
+}
