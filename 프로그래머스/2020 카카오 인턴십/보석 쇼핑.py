7caef318b632affbb3f3312e8197a478d670e6ab
wizdom-js <wltmfdl123@naver.com>
2022-08-11 23:40:17 +0900

    Comment : 주석달기

--- 프로그래머스/2020 카카오 인턴십/보석 쇼핑.py
+++ 프로그래머스/2020 카카오 인턴십/보석 쇼핑.py
@@ -1,33 +1,38 @@
 def solution(gems):
     n = len(gems)
     gem_kinds = len(set(gems))
-    
-    dic = {gems[0]:1}
+
+    # 보석의 빈도수 저장
+    dic = {gems[0]: 0}
     l, r = 0, 0
-    
+
     answer = [1, n]
-    while n > l :
-        if len(dic) != gem_kinds :
+    while n > l:
+        # 모든 종류의 보석이 들어오지 않았다면
+        if len(dic) != gem_kinds:
             r += 1
-            if r >= n :
+            # r이 보석의 끝인경우
+            if r >= n:
                 break
-            
-            if gems[r] in dic :
+
+            # 빈도수 넣어주기
+            if gems[r] in dic:
                 dic[gems[r]] += 1
-            else :
+            else:
                 dic[gems[r]] = 1
-                
-        else :
+
+        # 모든 종류의 보석이 다 들어온경우
+        else:
+            # 기존 구간 최단거리보다 현재 구간이 더 짧다면 갱신
             if r - l < answer[1] - answer[0]:
-                print('d')
-                print(r, l, answer[1], answer[0])
-                answer = [l+1, r+1]
-                
-            if dic[gems[l]] != 1 :
+                answer = [l + 1, r + 1]
+
+            # 보석의 빈도수 1 이상인 경우
+            if dic[gems[l]] != 1:
                 dic[gems[l]] -= 1
-            else :
-                del dic[gems[l]] 
-                
+            else:
+                del dic[gems[l]]
+
             l += 1
-                
+
     return answer
