f66fdd4a1d1ca0088dd983d9358b385f2ca5fbfb
wizdom-js <wltmfdl123@naver.com>
2022-08-11 23:39:30 +0900

    Python : 구현_Programmers

--- 알고리즘 스터디/DFS&BFS/17779_게리맨더링.py
+++ 알고리즘 스터디/DFS&BFS/17779_게리맨더링.py
@@ -1,25 +1,24 @@
 import sys
 sys.stdin = open('input.txt')
 
+
 def devideSection(x, y, d1, d2):
     global area, population
     area = [[0 for _ in range(n+1)] for _ in range(n+1)]
-    for i in range(1, d1+1):
-        print(x, y, d1, d2, i)
+    for i in range(d1+1):
         area[x+i][y-i] = 5
         area[x+d2+i][y+d2-i] = 5
 
-    for i in range(1, d2+1):
-        area[x+i][y-i] = 5
-        area[x+d1+i][y+d1-i] = 5
+    for i in range(d2+1):
+        area[x+i][y+i] = 5
+        area[x+d1+i][y-d1+i] = 5
 
     population = [0, 0, 0, 0, 0]
     inputSection(1, x+d1, 1, y+1, 0)
-    inputSection(1, x+d2+1, y+1, n+1, 1)
+    inputSection(1, x+d2+1, n, y, 1)
     inputSection(x+d1, n+1, 1, y-d1+d2, 2)
-    inputSection(x+d2+1, n+1, y-d1+d2, n+1, 3)
+    inputSection(x+d2+1, n+1, n, y-d1+d2-1, 3)
 
-    total_population = sum(sum(jaehyunsi[i]) for _ in range(n))
     population[4] = total_population - sum(population[:4])
 
     return max(population) - min(population)
@@ -27,17 +26,24 @@ def devideSection(x, y, d1, d2):
 
 
 def inputSection(x_s, x_e, y_s, y_e, section):
+    if section % 2:
+        backwards = -1
+    else:
+        backwards = 1
     for x in range(x_s, x_e):
-        for y in range(y_s, y_e):
+        for y in range(y_s, y_e, backwards):
             if area[x][y] == 5:
                 break
             population[section] += jaehyunsi[x][y]
 
 
+
 n = int(input())
-jaehyunsi = [[] for _ in range(n+1)]
+jaehyunsi = [[0 for _ in range(n+1)]] + [[] for _ in range(n)]
 for i in range(1, n+1):
     jaehyunsi[i] = [0] + list(map(int, input().split()))
+
+total_population = sum([sum(p) for p in jaehyunsi])
 
 answer = 999999999999999
 for x in range(1, n+1):
