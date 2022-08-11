be3da2050621af91ea36f4c9d141fd1caffd3535
wizdom-js <wltmfdl123@naver.com>
2022-08-12 00:00:01 +0900

    Python : DP_BOJ

---
+++ 알고리즘 스터디/DP/1749_점수따먹기.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt')
+
+n, m = map(int, input().split())
+numbers = [list(map(int, input().split())) for _ in range(n)]
+
+prefix_sum = [[0 for _ in range(m+1)] for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1] + numbers[i-1][j-1]
+
+answer = -9999999999999
+for x1 in range(1, n+1):
+    for y1 in range(1, n+1):
+        for x2 in range(x1, n+1):
+            for y2 in range(y1, n+1):
+                answer = max(answer, prefix_sum[x1-1][y1-1] - prefix_sum[y1-1][x2] - prefix_sum[y2][x1-1] + prefix_sum[y2][x2])
+
+print(answer)
