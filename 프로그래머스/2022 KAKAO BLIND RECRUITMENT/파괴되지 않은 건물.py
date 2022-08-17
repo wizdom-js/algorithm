acc37826c91981f139593a4dd5a0e78373c180a5
wizdom-js <wltmfdl123@naver.com>
2022-08-18 00:42:53 +0900

    Refactor : 코드 개선 및 주석

--- 프로그래머스/2022 KAKAO BLIND RECRUITMENT/파괴되지 않은 건물.py
+++ 프로그래머스/2022 KAKAO BLIND RECRUITMENT/파괴되지 않은 건물.py
@@ -1,39 +1,34 @@
 def solution(board, skill):
-    answer = 0
     h = len(board)
     w = len(board[0])
-    temp = [[0 for _ in range(w)] for _ in range(h)]
 
-    for [t, r1, c1, r2, c2, d] in skill:
-        if t == 1:
-            temp[r1][c1] -= d
-            if r2 + 1 < h and c2 + 1 < w:
-                temp[r2 + 1][c2 + 1] -= d
-            if r2 + 1 < h:
-                temp[r2 + 1][c1] += d
-            if c2 + 1 < w:
-                temp[r1][c2 + 1] += d
-        else:
-            temp[r1][c1] += d
-            if r2 + 1 < h and c2 + 1 < w:
-                temp[r2 + 1][c2 + 1] += d
-            if r2 + 1 < h:
-                temp[r2 + 1][c1] -= d
-            if c2 + 1 < w:
-                temp[r1][c2 + 1] -= d
+    tmp = [[0 for _ in range(w + 1)] for _ in range(h + 1)]  # 누적합 기록 배열
+    for typ, r1, c1, r2, c2, degree in skill:
+        if typ == 1:  # 공격 받은 경우 => 부호 반대로
+            degree *= -1
+        # 누적합 기록하기
+        tmp[r1][c1] += degree
+        tmp[r1][c2 + 1] += -degree
+        tmp[r2 + 1][c1] += -degree
+        tmp[r2 + 1][c2 + 1] += degree
 
-    for i in range(h):
-        for j in range(1, w):
-            temp[i][j] += temp[i][j - 1]
+    # 행 누적합
+    for y in range(h):
+        for x in range(w):
+            tmp[y][x + 1] += tmp[y][x]
 
-    for j in range(w):
-        for i in range(1, h):
-            temp[i][j] += temp[i - 1][j]
+    # 열 누적합
+    for y in range(h):
+        for x in range(w):
+            tmp[y + 1][x] += tmp[y][x]
 
-    for i in range(h):
-        for j in range(w):
-            board[i][j] += temp[i][j]
-            if board[i][j] > 0:
+    answer = 0
+    # 기존 내구도랑 합치기
+    for y in range(h):
+        for x in range(w):
+            board[y][x] += tmp[y][x]
+            # 내구도가 1 이상이면 정답 +1
+            if board[y][x] > 0:
                 answer += 1
 
     return answer
