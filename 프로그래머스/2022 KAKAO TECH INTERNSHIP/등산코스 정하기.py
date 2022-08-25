6098f4a45d24d79722e572f23e998cb21c39f7b8
wizdom-js <wltmfdl123@naver.com>
2022-08-25 23:52:14 +0900

    Refactor : 시간 복잡도 줄이기

--- 프로그래머스/2022 KAKAO TECH INTERNSHIP/등산코스 정하기.py
+++ 프로그래머스/2022 KAKAO TECH INTERNSHIP/등산코스 정하기.py
@@ -10,13 +10,21 @@ def solution(n, paths, gates, summits):
             heapq.heappush(queue, [0, gate])
             dist[gate] = 0
 
+        answer = [0, 9999999999]
+
         while queue:
             intensity, now = heapq.heappop(queue)
 
-            if now in summits:
+            if dist[now] < intensity:
                 continue
 
-            if dist[now] < intensity:
+            # 산봉우리인 경우
+            if now in summits:
+                if intensity < answer[1]:
+                    answer[0] = now
+                    answer[1] = intensity
+                elif intensity == answer[1]:
+                    answer[0] = min(answer[0], now)
                 continue
 
             for nxt, nxt_i in point_info[now]:
@@ -25,11 +33,14 @@ def solution(n, paths, gates, summits):
                     dist[nxt] = choosed_i
                     heapq.heappush(queue, [choosed_i, nxt])
 
-        answer = [0, 9999999999]
-        for summit in summits:
-            if dist[summit] < answer[1]:
-                answer[0] = summit
-                answer[1] = dist[summit]
+        # 처음 풀었던 방식 => summit이 숫자 크기대로 주어진게 아니라서 한번 정렬을 해야하고 summit을 한번 더 돌아야하기 때문에 시간초과가 난다.
+        # answer = [0, 9999999999]
+        # for summit in summits:
+        #     if dist[summit] < answer[1]:
+        #         answer[0] = summit
+        #         answer[1] = dist[summit]
+        #
+        # return answer
 
         return answer
 
@@ -40,7 +51,4 @@ def solution(n, paths, gates, summits):
         point_info[point2].append([point1, time])
 
     return find_intensity()
-
-
-
 
