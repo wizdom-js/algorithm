d75d999e38299d75a6e55ad8ce3e8b908a29e5b0
wizdom-js <wltmfdl123@naver.com>
2022-08-26 23:58:37 +0900

    Refactor : 산봉우리 체크 비트로 변경 및 주석 추가

--- 프로그래머스/2022 KAKAO TECH INTERNSHIP/등산코스 정하기.py
+++ 프로그래머스/2022 KAKAO TECH INTERNSHIP/등산코스 정하기.py
@@ -6,6 +6,7 @@ def solution(n, paths, gates, summits):
         dist = [float('inf') for _ in range(n + 1)]
 
         queue = []
+        # 시작점 모두 넣기 => 어디서 시작하든 상관 없으므로
         for gate in gates:
             heapq.heappush(queue, [0, gate])
             dist[gate] = 0
@@ -15,11 +16,13 @@ def solution(n, paths, gates, summits):
         while queue:
             intensity, now = heapq.heappop(queue)
 
+            # 이미 더 가까운 거리가 있다면 패스
             if dist[now] < intensity:
                 continue
 
             # 산봉우리인 경우
-            if now in summits:
+            # if now in summits:
+            if summits_bit & (1 << now):
                 if intensity < answer[1]:
                     answer[0] = now
                     answer[1] = intensity
@@ -27,7 +30,10 @@ def solution(n, paths, gates, summits):
                     answer[0] = min(answer[0], now)
                 continue
 
+            # 현재(now) 지점에서 이어진 다음 지점 가보기
             for nxt, nxt_i in point_info[now]:
+                # intensity가 가장 최소가 되도록 등산코스를 정해야 함
+                # 그리고 현재 등산코스에서 가장 큰 intensity를 찾아야 하니 더 큰 것을 선택
                 choosed_i = nxt_i if nxt_i > intensity else intensity
                 if choosed_i < dist[nxt]:
                     dist[nxt] = choosed_i
@@ -50,5 +56,9 @@ def solution(n, paths, gates, summits):
         point_info[point1].append([point2, time])
         point_info[point2].append([point1, time])
 
-    return find_intensity()
+    # 산봉우리 확인하기 위한 비트
+    summits_bit = 0
+    for i in summits:
+        summits_bit |= 1 << i
 
+    return find_intensity()
