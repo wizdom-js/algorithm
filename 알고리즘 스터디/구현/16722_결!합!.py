3182a3490cff20677bb4574776e69b7a597d18c9
wizdom-js <wltmfdl123@naver.com>
2022-09-03 23:42:28 +0900

    Python : Queue_Programmers

--- 알고리즘 스터디/구현/16722_결!합!.py
+++ 알고리즘 스터디/구현/16722_결!합!.py
@@ -17,36 +17,21 @@ def input_board(idx, s, c, b):
     elif b == "BLACK":
         board[idx][2] = 2
 
-def hab(n1, n2, n3):
-    global score
-    for i in range(3):
-        if board[n1][i] == board[n2][i] != board[n3][i]:
-            continue
-        elif board[n1][i] != board[n2][i] == board[n3][i]:
-            continue
-        elif board[n1][i] != board[n3][i] == board[n2][i]:
-            continue
-        else:
-            score += 1
-
 
 board = [[0, 0, 0] for _ in range(10)]
 
-for i in range(9):
+for i in range(1, 10):
     s, c, b = input().split()
     input_board(i, s, c, b)
-
 
 hab_comb = {}
 for i in range(1, 8):
     for j in range(i+1, 9):
         for k in range(j+1, 10):
             for z in range(3):
-                if board[i][z] == board[j][z] != board[k][z]:
-                    break
-                elif board[i][z] != board[j][z] == board[k][z]:
-                    break
-                elif board[i][z] != board[j][z] == board[k][z]:
+                if board[i][z] == board[j][z] == board[k][z] or board[i][z] != board[j][z] != board[k][z] != board[i][z]:
+                    continue
+                else:
                     break
             else:
                 tmp = str(i) + str(j) + str(k)
@@ -54,21 +39,15 @@ for i in range(1, 8):
 
 game_n = int(input())
 score = 0
-record = {}
-print(hab_comb)
 for _ in range(game_n):
     player = input()
-    print('------', player)
     if player[0] == "H":
         tmp = sorted(player[1:].split())
         cards = ''.join(tmp)
-        print(cards)
         if cards in hab_comb and not hab_comb[cards]:
             hab_comb[cards] = True
-            print("true")
             score += 1
         else:
-            print("false")
             score -= 1
     else:
         for value in hab_comb.values():
@@ -76,6 +55,6 @@ for _ in range(game_n):
                 score -= 1
                 break
         else:
-            score += 1
+            score += 3
 
 print(score)
