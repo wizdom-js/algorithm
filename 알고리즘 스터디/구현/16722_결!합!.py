24f43910a9a5f2c4ef40865a01d7916caa2093ad
wizdom-js <wltmfdl123@naver.com>
2022-09-04 21:54:53 +0900

    Python : 구현_BOJ

--- 알고리즘 스터디/구현/16722_결!합!.py
+++ 알고리즘 스터디/구현/16722_결!합!.py
@@ -1,17 +1,21 @@
 import sys
 sys.stdin = open('input.txt')
 
+# 보드에 도형 숫자형태로 채워넣기
 def input_board(idx, s, c, b):
+    # 모양 - 0: 동그라미, 1: 세모, 2: 네모
     if s == "TRIANGLE":
         board[idx][0] = 1
     elif s == "SQUARE":
         board[idx][0] = 2
 
+    # 색 - 0: 노랑, 1: 빨강, 2: 파랑
     if c == "RED":
         board[idx][1] = 1
     elif c == "BLUE":
         board[idx][1] = 2
 
+    # 배경색 - 0: 회색, 1: 흰색, 2: 검은색
     if b == "WHITE":
         board[idx][2] = 1
     elif b == "BLACK":
@@ -24,23 +28,27 @@ for i in range(1, 10):
     s, c, b = input().split()
     input_board(i, s, c, b)
 
+# 합이되는 조합 먼저 만들어 놓기
 hab_comb = {}
 for i in range(1, 8):
     for j in range(i+1, 9):
         for k in range(j+1, 10):
             for z in range(3):
+                # 세 가지 속성이 모두 같거나 모두 다르다면 '합'
                 if board[i][z] == board[j][z] == board[k][z] or board[i][z] != board[j][z] != board[k][z] != board[i][z]:
                     continue
                 else:
                     break
-            else:
+            else:   # 합인 경우 조합 넣기
                 tmp = str(i) + str(j) + str(k)
                 hab_comb[tmp] = False
 
 game_n = int(input())
 score = 0
+gyul_flag = False   # 결을 외쳤는지 확인하는 플래그
 for _ in range(game_n):
     player = input()
+    # 합을 외친 경우
     if player[0] == "H":
         tmp = sorted(player[1:].split())
         cards = ''.join(tmp)
@@ -49,12 +57,17 @@ for _ in range(game_n):
             score += 1
         else:
             score -= 1
+    # 결을 외친 경우
     else:
         for value in hab_comb.values():
-            if not value:
+            if not value:   # 외치지 않은 합이 있을 경우
                 score -= 1
                 break
-        else:
-            score += 3
+        else:   # 외치지 않은 합이 없는 경우
+            if not gyul_flag:
+                score += 3
+                gyul_flag = True
+            else:
+                score -= 1
 
 print(score)
