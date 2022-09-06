171e71b63c963900f75eabed293c2083929f7ef5
wizdom-js <wltmfdl123@naver.com>
2022-09-06 23:48:00 +0900

    Comment : 풀이 주석 달기

--- 프로그래머스/2022 KAKAO TECH INTERNSHIP/두 큐 합 같게 만들기.py
+++ 프로그래머스/2022 KAKAO TECH INTERNSHIP/두 큐 합 같게 만들기.py
@@ -2,23 +2,26 @@ from collections import deque
 
 
 def solution(queue1, queue2):
-    answer = 0
     q1 = deque(queue1)
     q2 = deque(queue2)
     sum_q1 = sum(q1)
-    sum_q2 = sum(q1)
-    for i in range(len(queue1) * 3):
-        if sum_q1 == sum_q2:
+    sum_q2 = sum(q2)
+
+    # 최대 이동 횟수 => q1 * 3 - 2
+    # 큰 수가 몰려 있는 경우를 생각하면 된다. 
+    # [1,1,1,8,10,9] [1,1,1,1,1,1] => 15회
+    for i in range(len(q1) * 3 - 2):
+        if sum_q1 == sum_q2:    # 원소 합이 같은 경우
             return i
-        elif sum_q1 > sum_q2:
-            num = q1.popleft()
-            q2.append(num)
-            sum_q1 -= num
-            sum_q2 += num
-        else:
-            num = q2.popleft()
-            q1.append(num)
-            sum_q1 += num
-            sum_q2 -= num
+        elif sum_q1 > sum_q2:   # q1이 큰 경우
+            number = q1.popleft()
+            q2.append(number)
+            sum_q1 -= number
+            sum_q2 += number
+        else:                   # q2합이 큰 경우
+            number = q2.popleft()
+            q1.append(number)
+            sum_q1 += number
+            sum_q2 -= number
 
     return -1
