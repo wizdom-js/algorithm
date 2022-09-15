d51c13307349f0be3487caa1bab9d913ea24537f
wizdom-js <wltmfdl123@naver.com>
2022-09-15 23:21:33 +0900

    Refactor : 코드정리

--- 프로그래머스/다단계 칫솔 판매.py
+++ 프로그래머스/다단계 칫솔 판매.py
@@ -1,13 +1,16 @@
 def solution(enroll, referral, seller, amount):
-    money = [0 for _ in range(len(enroll))]
+    answer = [0 for _ in range(len(enroll))]
     name_idx = dict()
-    for i, e in enumerate(enroll):
-        name_idx[e] = i
+
+    for i in range(len(enroll)):
+        name_idx[enroll[i]] = i
+
     for s, a in zip(seller, amount):
         m = a * 100
         while s != "-" and m > 0:
             idx = name_idx[s]
-            money[idx] += m - m//10
+            answer[idx] += m - m // 10
             m //= 10
             s = referral[idx]
-    return money
+
+    return answer
