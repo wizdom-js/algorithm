708bb8a96be54d07e0c2668be57deaa3072a97d7
wizdom-js <wltmfdl123@naver.com>
2022-09-22 23:19:23 +0900

    Java : 기초_Programmers

---
+++ JAVA/Programmers/연습문제/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        double sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum / arr.length;
+    }
+}
