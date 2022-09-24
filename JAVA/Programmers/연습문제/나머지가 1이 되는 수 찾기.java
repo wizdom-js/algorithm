0ea149da597d7c3e201d4bc2c64a0d12d32d26b9
wizdom-js <wltmfdl123@naver.com>
2022-09-24 23:29:38 +0900

    Java : 기초_Programmers

---
+++ JAVA/Programmers/연습문제/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 2;
+        while (true) {
+            if (n % answer == 1) {
+                break;
+            } else {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
