18c8d3edffb69f092e527848f0b36974b0b983a2
wizdom-js <wltmfdl123@naver.com>
2022-09-24 23:58:22 +0900

    Refactor : 복습 & 코드 정리

--- JAVA/Programmers/완주하지 못한 선수.java
+++ JAVA/Programmers/완주하지 못한 선수.java
@@ -1,24 +1,59 @@
 import java.util.*;
 
+// 처음 풀었던 내용
+import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Map<String, Integer> hm = new HashMap<>();
-        
-        for (String pc : participant){
-            hm.put(pc, hm.getOrDefault(pc, 0)+1);
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>(participant.length);
+        for (String p : participant) {
+            if (hashMap.containsKey(p)) {
+                int cnt = hashMap.get(p) + 1;
+                hashMap.replace(p, cnt);
+            } else {
+                hashMap.put(p, 1);
+            }
         }
-        
-        for (String cp : completion){
-            hm.put(cp, hm.get(cp)-1);
+
+        for (String c : completion) {
+            int cnt = hashMap.get(c);
+            hashMap.replace(c, cnt - 1);
         }
-        
-        for (String key : hm.keySet()){
-            if (hm.get(key) != 0) {
-                answer = key;
+
+        for (String p : hashMap.keySet()) {
+            int cnt = hashMap.get(p);
+            if (cnt > 0) {
+                answer = p;
                 break;
             }
         }
+
+        return answer;
+    }
+}
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> hashMap = new HashMap<>(participant.length);
+        for (String p : participant) {
+            hashMap.put(p, hashMap.getOrDefault(p, 0) + 1);
+        }
+
+        for (String c : completion) {
+            int cnt = hashMap.get(c);
+            hashMap.replace(c, cnt - 1);
+        }
+
+        for (String p : hashMap.keySet()) {
+            int cnt = hashMap.get(p);
+            if (cnt > 0) {
+                answer = p;
+                break;
+            }
+        }
+
         return answer;
     }
 }
