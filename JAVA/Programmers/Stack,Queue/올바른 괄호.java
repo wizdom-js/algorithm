aedbe08e5f5e931effd62a9396fe40168664c957
wizdom-js <wltmfdl123@naver.com>
2022-09-30 22:16:02 +0900

    Refactor : Stack -> count

--- JAVA/Programmers/Stack,Queue/올바른 괄호.java
+++ JAVA/Programmers/Stack,Queue/올바른 괄호.java
@@ -23,3 +23,23 @@ class Solution {
         return stack.empty() ? true : false;
     }
 }
+
+// 스택 쓰지 않은 풀이
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return count == 0 ? true : false;
+    }
+}
