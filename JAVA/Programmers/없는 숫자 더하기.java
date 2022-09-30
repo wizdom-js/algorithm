08a65fbc74847fc01c2ba7bc1065d092485ab74b
wizdom-js <wltmfdl123@naver.com>
2022-09-30 21:47:20 +0900

    Refactor : 코드 정리

--- JAVA/Programmers/없는 숫자 더하기.java
+++ JAVA/Programmers/없는 숫자 더하기.java
@@ -1,9 +1,9 @@
-public class Solution {
-  public int solution(int[] numbers) {
-      int answer = 45;
-      for (int n : numbers) {
-          answer -= n;
-      }
-
-      return answer;
-  }
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for (int number : numbers) {
+            answer -= number;
+        }
+        return answer;
+    }
+}
