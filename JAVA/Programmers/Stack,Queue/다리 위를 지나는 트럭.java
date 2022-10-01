602be69413ccbe2628ce140f82d77a4f0ad4729d
wizdom-js <wltmfdl123@naver.com>
2022-10-01 23:12:48 +0900

    Comment : 변수 정리, 주석 달기

--- JAVA/Programmers/Stack,Queue/다리 위를 지나는 트럭.java
+++ JAVA/Programmers/Stack,Queue/다리 위를 지나는 트럭.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
+        int time = 0;
         Queue<Integer> bridge = new LinkedList<>();
         int bridge_weight = 0;
 
         for (int truck : truck_weights) {
             while (true) {
-                if (bridge.size() < bridge_length) {
-                    if (bridge_weight + truck <= weight) {
+                if (bridge.size() < bridge_length) {    // 다리 꽉 차있지 않은 경우
+                    if (bridge_weight + truck <= weight) {  // 무게 때문에 트럭 올라갈 수 있는 경우
                         bridge.offer(truck);
                         bridge_weight += truck;
-                        answer++;
+                        time++;
                         break;
-                    } else {
+                    } else {    // 트럭 올라가지 못하는 경우
                         bridge.offer(0);
-                        answer++;
+                        time++;
                     }
                 } else {
                     bridge_weight -= bridge.poll();
                 }
             }
         }
-        return answer + bridge_length;
+        return time + bridge_length;
     }
 }
