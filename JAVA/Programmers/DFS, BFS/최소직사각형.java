aed864bb1ed57ce6a85591ed159f77946445a2a2
wizdom-js <wltmfdl123@naver.com>
2022-10-12 00:21:22 +0900

    Refactor : 풀이 고치기 및 주석달기

--- JAVA/Programmers/DFS, BFS/최소직사각형.java
+++ JAVA/Programmers/DFS, BFS/최소직사각형.java
@@ -4,16 +4,22 @@ class Solution {
         int width = 0;
         int height = 0;
         for (int[] size : sizes) {
-            int w = Math.min(size[0], size[1]);
-            int h = Math.max(size[0], size[0]);
-            if (width < w ) {
-                width = w;
+            // 길이가 더 긴 곳을 세로로 배치 은
+            // => 짧은 길이는 짧 길이 끼리, 긴 길이는 긴 길이 끼리 비교
+            int w;
+            int h;
+            if (size[0] < size[1]) {
+                w = size[0];
+                h = size[1];
+            } else {
+                w = size[1];
+                h = size[0];
             }
 
-            if (height < h) {
-                height = h;
-            }
+            width = Math.max(width, w);
+            height = Math.max(height, h);
         }
+
         answer = width * height;
         return answer;
     }
