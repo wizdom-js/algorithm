34c174550509a11fd8df0d60e72e8a0a41a75129
wizdom-js <wltmfdl123@naver.com>
2022-10-14 23:59:46 +0900

    Comment : 주석 달기

--- 알고리즘 스터디/DP/14501_퇴사.py
+++ 알고리즘 스터디/DP/14501_퇴사.py
@@ -5,16 +5,13 @@ n = int(input())
 schedule = [list(map(int, input().split())) for _ in range(n)]
 
 dp = [0 for _ in range(n+1)]
+# 탑 다운 방식
 for i in range(n-1, -1, -1):
     day = schedule[i][0]
     price = schedule[i][1]
-    if i + day <= n:
-        dp[i] = max(dp[i+1], dp[i + day] + price)
-    else:
+    if i + day <= n:    # 상담 일이 기간 내라면
+        dp[i] = max(dp[i+1], dp[i + day] + price)   # 지금 상담 선택 안하기 vs 지금 상담 비용 + 지금 상담 후 날짜의 비용
+    else:               # 기간 넘는 경우
         dp[i] = dp[i+1]
-
-
-    print(day, price)
-    print(i, dp)
 
 print(dp[0])
--- 알고리즘 스터디/DP/2725_보이는 점의 개수.py
+++ 알고리즘 스터디/DP/2725_보이는 점의 개수.py
@@ -1,6 +1,10 @@
 import sys
 sys.stdin = open('input.txt')
 
+# (0, 0)에서 (1, 2)가 보이고 (2, 4), (3, 6)과 같은 좌표는 (1, 2)에 가려져서 보이지 않는다.
+# 따라서 최대 공약수가 1인 경우에만 (0, 0)좌표에서 보인다.
+
+# 최대 공약수 구하기
 def get_GCD(a, b):
     while a % b != 0:
         a, b = b, a % b
@@ -8,6 +12,7 @@ def get_GCD(a, b):
     return b
 
 # dp 풀이
+# dp에 값들 먼저 다 저장해놓고 꺼내 쓰는 방식
 dp = [0 for _ in range(1001)]
 dp[1] = 3
 for i in range(2, 1001):
@@ -16,8 +21,8 @@ for i in range(2, 1001):
         if i == j: continue
 
         if get_GCD(i, j) == 1:
-            cnt += 2
-    dp[i] = dp[i - 1] + cnt
+            cnt += 2    # 예) (2, 3) 와 (3, 2)
+    dp[i] = dp[i - 1] + cnt # 이전 점들의 개수 + 추가된 점의 개수
 
 testcase = int(input())
 for _ in range(testcase):
@@ -25,7 +30,7 @@ for _ in range(testcase):
     print(dp[n])
 
 
-# 처음 풀이 (구현)
+# 처음 풀이 (구현) => 시간초과
 '''
 testcase = int(input())
 arr = [[0 for _ in range(1001)] for _ in range(1001)]
@@ -39,6 +44,7 @@ for _ in range(testcase):
 
             if get_GCD(i, j) == 1:
                 answer += 1
+                # 배수에 해당되는 좌표들 처리하기 
                 for k in range(1, 1000):
                     if i * k > 1000 or j * k > 1000:
                         break
