4e11b8e8dd91034964fb67c2cff527574933698a
wizdom-js <wltmfdl123@naver.com>
2022-10-18 22:11:54 +0900

    Python : String_Programmers

--- 프로그래머스/2019 카카오 개발자 겨울 인턴십/튜플.py
+++ 프로그래머스/2019 카카오 개발자 겨울 인턴십/튜플.py
@@ -15,3 +15,37 @@ def solution(s):
                 answer.append(int(i[j]))
 
     return answer
+
+
+def solution(s):
+    set_list = []
+    idx = 1
+    while idx < len(s):
+        tmp = []
+        number = ""
+        while True:
+            idx += 1
+            if s[idx] == ",":
+                tmp.append(int(number))
+                number = ""
+            elif s[idx] == "}":
+                tmp.append(int(number))
+                idx += 1
+                break
+            else:
+                number += s[idx]
+
+        set_list.append(tmp)
+        idx += 1
+
+    set_list.sort(key=lambda x: len(x))
+
+    num_dict = dict()
+    answer = set()
+    for num_set in set_list:
+        for num in num_set:
+            if not num in num_dict:
+                num_dict[num] = True
+                answer.append(num)
+
+    return answer
