f6c3c22c7fc2a1a344c076cdc784622a5e4f4188
wizdom-js <wltmfdl123@naver.com>
2022-10-23 18:43:12 +0900

    Refactor : 정리 코드 추가(더 느림)

--- 알고리즘 스터디/DFS&BFS/5014_스타트링크.py
+++ 알고리즘 스터디/DFS&BFS/5014_스타트링크.py
@@ -13,16 +13,25 @@ def bfs():
         if floor == startlink:
             return cnt
 
+        # 길지만 빠름
         move_up = floor + up_button
-        move_down = floor - down_button
         if move_up <= height and not visited[move_up]:
             visited[move_up] = True
             queue.append([move_up, cnt + 1])
+
+        move_down = floor - down_button
         if move_down > 0 and not visited[move_down]:
             visited[move_down] = True
             queue.append([move_down, cnt + 1])
 
+        # 짧지만 느림
+        # for f in (move_up, move_down):
+        #     if 0 < f <= height and not visited[f]:
+        #         visited[f] = True
+        #         queue.append([f, cnt + 1])
+
     return "use the stairs"
+
 
 height, gangho, startlink, up_button, down_button = map(int, input().split())
 visited = [0 for _ in range(height + 1)]
