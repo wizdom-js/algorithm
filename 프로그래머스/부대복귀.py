478b5d70122896bf8f588a613c9ca7553bdad54f
wizdom-js <wltmfdl123@naver.com>
2022-11-01 00:07:10 +0900

    Refactor : 시간 단축 풀이 추가

--- 프로그래머스/부대복귀.py
+++ 프로그래머스/부대복귀.py
@@ -1,6 +1,31 @@
 from collections import deque
 
+from collections import deque
 
+
+# 더 빠른 풀이
+def solution(n, roads, sources, destination):
+    area_info = [[] for _ in range(n + 1)]
+    for area1, area2 in roads:
+        area_info[area1].append(area2)
+        area_info[area2].append(area1)
+
+    distance = [-1 for _ in range(n + 1)]
+    distance[destination] = 0
+    queue = deque([destination])
+
+    while queue:
+        now = queue.popleft()
+
+        for nxt in area_info[now]:
+            if distance[nxt] == -1:
+                distance[nxt] = distance[now] + 1
+                queue.append(nxt)
+
+    answer = [distance[s] for s in sources]
+    return answer
+
+# 처음 풀이
 def solution(n, roads, sources, destination):
     def bfs(s):
         queue = deque([[s, 0, [0]]])
