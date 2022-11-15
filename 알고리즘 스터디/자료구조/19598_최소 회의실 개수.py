868298ab6c0e13bc8f054c85593ecf591820e1b9
wizdom-js <wltmfdl123@naver.com>
2022-11-15 20:02:00 +0900

    Refactor : heappop, heappush => heapreplace로 대체

--- 알고리즘 스터디/자료구조/19598_최소 회의실 개수.py
+++ 알고리즘 스터디/자료구조/19598_최소 회의실 개수.py
@@ -10,8 +10,14 @@ meetings.sort()
 
 meeting_rooms = [meetings[0][1]]
 for meeting in meetings[1:]:
-    if meeting[0] >= meeting_rooms[0]:
-        heapq.heappop(meeting_rooms)
-    heapq.heappush(meeting_rooms, meeting[1])
+    # if meeting[0] >= meeting_rooms[0]:
+    #     heapq.heappop(meeting_rooms)
+    # heapq.heappush(meeting_rooms, meeting[1])
+
+    # heapreplace => heappush, heappop 두 연산을 하는 것보다 효율적이다. 
+    if meeting[0] < meeting_rooms[0]:
+        heapq.heappush(meeting_rooms, meeting[1])
+    else:
+        heapq.heapreplace(meeting_rooms, meeting[1])
 
 print(len(meeting_rooms))
