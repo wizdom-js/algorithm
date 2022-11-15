217e1d2e46b408363669ba8271e4e986b4af68e2
wizdom-js <wltmfdl123@naver.com>
2022-11-15 19:47:48 +0900

    Refactor : replace -> pop, push 변경

--- 알고리즘 스터디/자료구조/19598_최소 회의실 개수.py
+++ 알고리즘 스터디/자료구조/19598_최소 회의실 개수.py
@@ -3,16 +3,15 @@ sys.stdin = open('input.txt')
 
 import heapq
 
-N = int(input())
-meetings = [list(map(int, input().split())) for _ in range(N)]
+n = int(input())
+meetings = [list(map(int, input().split())) for _ in range(n)]
 
 meetings.sort()
 
 meeting_rooms = [meetings[0][1]]
 for meeting in meetings[1:]:
-    if meeting_rooms[0] > meeting[0]:
-        heapq.heappush(meeting_rooms, meeting[1])
-    else:
-        heapq.heapreplace(meeting_rooms, meeting[1])
+    if meeting[0] >= meeting_rooms[0]:
+        heapq.heappop(meeting_rooms)
+    heapq.heappush(meeting_rooms, meeting[1])
 
 print(len(meeting_rooms))
