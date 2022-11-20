6064ee9827d390ca7ba353c2f8410794d4a36a66
wizdom-js <wltmfdl123@naver.com>
2022-11-20 23:28:06 +0900

    Comment : 코드 정리 및 주석

--- 알고리즘 스터디/구현/1325_효율적인 해킹.py
+++ 알고리즘 스터디/구현/1325_효율적인 해킹.py
@@ -13,6 +13,7 @@ def dfs(com):
             # visited[nxt_com] = 0
 
 
+# 해킹할 수 있는 모든 곳 다 가보고 방문처리하기
 def bfs(s):
     visited = [0 for _ in range(n+1)]
     visited[s] = True
@@ -26,7 +27,9 @@ def bfs(s):
                 visited[nxt] = True
                 queue.append(nxt)
 
+    # 방문한 곳 => 해킹한 컴퓨터 이므로 방문 개수를 반환
     return sum(visited)
+
 
 n, m = map(int, input().split())
 
@@ -34,30 +37,19 @@ graph = [[] for _ in range(n+1)]
 # trust = [0 for _ in range(n+1)]
 for _ in range(m):
     com1, com2 = map(int, input().split())
-    graph[com2].append(com1)
+    graph[com2].append(com1)    # com2에서 com1을 감염 시킬(방문 할) 수 있다.
     # trust[com2] += 1
-
-# print(trust)
 
 answer = []
 max_v = 0
 for i in range(1, n+1):
     tmp_v = bfs(i)
-    if max_v < tmp_v:
-        answer = [i]
-        max_v = tmp_v
-    elif max_v == tmp_v:
+
+    if max_v < tmp_v:   # 해킹할 수 있는 컴퓨터 수가 더 많은 경우
+        answer = [i]    # 배열 초기화
+        max_v = tmp_v   # 업데이트
+    elif max_v == tmp_v:    # 수가 같은 경우에는 추가
         answer.append(i)
-#
-# print(trust)
-# print(graph)
-
-# max_v = max(trust)
-
-# for i in range(1, n+1):
-#     if max_v == trust[i]:
-#         print(i, end=" ")
-
 
 print(*answer)
 
