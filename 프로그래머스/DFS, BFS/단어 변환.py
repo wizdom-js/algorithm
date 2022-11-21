b63ea7dcbdf33522452658b68267e5bcabc36ab0
wizdom-js <wltmfdl123@naver.com>
2022-11-21 21:07:04 +0900

    Refactor : 공통 코드 함수로 변경 및 전역 변수 활용 / 주석 달기

--- 프로그래머스/DFS, BFS/단어 변환.py
+++ 프로그래머스/DFS, BFS/단어 변환.py
@@ -1,25 +1,39 @@
 from collections import deque
 
+word_l = 0
+
+
+# 해당 단어로 변환 가능한지 검토 (알파벳 한 개만 다른지 검토)
+def possible_change(before_word, after_word):
+    diff = 0
+    for i in range(word_l):
+        if before_word[i] != after_word[i]:
+            diff += 1
+
+    if diff == 1:   # 알파벳 한 개만 다르다면
+        return True
+    return False
+
+
 def solution(begin, target, words):
-    if target not in words:
-        return 0
+    global word_l
 
     word_n = len(words)
+    word_l = len(words[0])
 
     queue = deque([[begin, 0]])
 
+    visited = [0 for _ in range(word_n)]
     while queue:
-        x, cnt = queue.popleft()
+        word, cnt = queue.popleft()
 
-        if x == target:
+        if word == target:
             return cnt
 
         for i in range(word_n):
-            diff = 0
-            word = words[i]
-            for j in range(len(x)):
-                if x[j] != word[j]:
-                    diff += 1
-            if diff == 1:
-                queue.append([word, cnt + 1])
-    return 0
+            if not visited[i]:  # 해당 단어로 변환한 적 없는 경우
+                if possible_change(word, words[i]):
+                    visited[i] = True
+                    queue.append([words[i], cnt + 1])
+
+    return 0    # 변환할 수 없는 경우
