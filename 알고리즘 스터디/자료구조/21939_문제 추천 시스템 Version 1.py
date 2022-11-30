55f26fc7393a37324d041119eba31c46ccd79877
wizdom-js <wltmfdl123@naver.com>
2022-11-30 23:58:58 +0900

    Refactor : min heap, max heap 따로 두어 시간 단축하기

--- 알고리즘 스터디/자료구조/21939_문제 추천 시스템 Version 1.py
+++ 알고리즘 스터디/자료구조/21939_문제 추천 시스템 Version 1.py
@@ -4,32 +4,41 @@ sys.stdin = open('input.txt')
 import heapq
 
 problem_n = int(input())
-problem_list = []
+min_heap = []
+max_heap = []
 for _ in range(problem_n):
     problem_number, problem_level = map(int, input().split())
-    heapq.heappush(problem_list, [problem_level, problem_number])
+    heapq.heappush(min_heap, [problem_level, problem_number])
+    heapq.heappush(max_heap, [-problem_level, -problem_number])
 
 command_n = int(input())
 for _ in range(command_n):
     command = list(input().split())
     if command[0] == "add":
-        heapq.heappush(problem_list, [int(command[2]), int(command[1])])
+        problem_number, problem_level = int(command[1]), int(command[2])
+        heapq.heappush(min_heap, [problem_level, problem_number])
+        heapq.heappush(max_heap, [-problem_level, -problem_number])
     elif command[0] == "solved":
         target_problem_number = int(command[1])
-        for i in range(len(problem_list)):
-            if problem_list[i][1] == target_problem_number:
-                problem_list = problem_list[:i] + problem_list[i+1:]
-                break
+        heap_length = len(min_heap)
+        min_flag = True
+        max_flag = True
+        for i in range(heap_length):
+            if min_flag and min_heap[i][1] == target_problem_number:
+                min_heap = min_heap[:i] + min_heap[i+1:]
+                min_flag = False
+            if max_flag and max_heap[i][1] == -target_problem_number:
+                max_heap = max_heap[:i] + max_heap[i+1:]
+                max_flag = False
     elif command[0] == "recommend":
         if command[1] == "1":
-            # tmp = heapq.nlargest(len(problem_list), problem_list)[1:]
-            # heapq.heapify(tmp)
-            # print(tmp, problem_list)
-            # print(problem_list[0][1])
-            # heapq.heappush(tmp, problem_list[0])
-            # problem_list = tmp
-            print(heapq.nlargest(1, problem_list)[0][1])
+            hardest_problem = heapq.heappop(max_heap)
+            print(-hardest_problem[1])
+            heapq.heappush(max_heap, hardest_problem)
         elif command[1] == "-1":
-            print(heapq.nsmallest(1, problem_list)[0][1])
+            easiest_problem = heapq.heappop(min_heap)
+            print(easiest_problem[1])
+            heapq.heappush(min_heap, easiest_problem)
+
 
 
