ef39e5b5326d114e96f592a7fa822192b8410cb5
wizdom-js <wltmfdl123@naver.com>
2022-12-03 23:55:04 +0900

    Refactor : 딕셔너리 활용하여 시간단축

--- 알고리즘 스터디/자료구조/21939_문제 추천 시스템 Version 1.py
+++ 알고리즘 스터디/자료구조/21939_문제 추천 시스템 Version 1.py
@@ -2,43 +2,41 @@ import sys
 sys.stdin = open('input.txt')
 
 import heapq
+input = sys.stdin.readline
 
 problem_n = int(input())
 min_heap = []
 max_heap = []
+is_solved = dict()
 for _ in range(problem_n):
     problem_number, problem_level = map(int, input().split())
-    heapq.heappush(min_heap, [problem_level, problem_number])
-    heapq.heappush(max_heap, [-problem_level, -problem_number])
+    heapq.heappush(min_heap, (problem_level, problem_number))
+    heapq.heappush(max_heap, (-problem_level, -problem_number))
+    is_solved[problem_number] = False
 
 command_n = int(input())
 for _ in range(command_n):
     command = list(input().split())
     if command[0] == "add":
         problem_number, problem_level = int(command[1]), int(command[2])
-        heapq.heappush(min_heap, [problem_level, problem_number])
-        heapq.heappush(max_heap, [-problem_level, -problem_number])
+        while is_solved[-max_heap[0][1]]:
+            heapq.heappop(max_heap)
+        while is_solved[min_heap[0][1]]:
+            heapq.heappop(min_heap)
+        heapq.heappush(min_heap, (problem_level, problem_number))
+        heapq.heappush(max_heap, (-problem_level, -problem_number))
+        is_solved[problem_number] = False
     elif command[0] == "solved":
-        target_problem_number = int(command[1])
-        heap_length = len(min_heap)
-        min_flag = True
-        max_flag = True
-        for i in range(heap_length):
-            if min_flag and min_heap[i][1] == target_problem_number:
-                min_heap = min_heap[:i] + min_heap[i+1:]
-                min_flag = False
-            if max_flag and max_heap[i][1] == -target_problem_number:
-                max_heap = max_heap[:i] + max_heap[i+1:]
-                max_flag = False
+        is_solved[int(command[1])] = True
     elif command[0] == "recommend":
         if command[1] == "1":
-            hardest_problem = heapq.heappop(max_heap)
-            print(-hardest_problem[1])
-            heapq.heappush(max_heap, hardest_problem)
+            while is_solved[-max_heap[0][1]]:
+                heapq.heappop(max_heap)
+            print(-max_heap[0][1])
         elif command[1] == "-1":
-            easiest_problem = heapq.heappop(min_heap)
-            print(easiest_problem[1])
-            heapq.heappush(min_heap, easiest_problem)
+            while is_solved[min_heap[0][1]]:
+                heapq.heappop(min_heap)
+            print(min_heap[0][1])
 
 
 
