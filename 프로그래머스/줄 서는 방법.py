b5539039f3371d24f80cb073c2c809e16025fecc
wizdom-js <wltmfdl123@naver.com>
2022-12-11 23:54:05 +0900

    Python : Math_Programmers

---
+++ 프로그래머스/줄 서는 방법.py
@@ -0,0 +1,19 @@
+import math
+
+def solution(n, k):
+    answer = []
+    number_list = [i for i in range(1, n + 1)]
+
+    while n:
+        temp = math.factorial(n) // n
+        idx = k // temp
+        k = k % temp
+        
+        if k == 0:
+            answer.append(number_list.pop(idx - 1))
+        else:
+            answer.append(number_list.pop(idx))
+
+        n -= 1
+
+    return answer
